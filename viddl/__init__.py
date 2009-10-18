05bcbc5f1d66ccbc5b6e218c68039da8c4302bab
Lee Begg <llnz@paradise.net.nz>
2009-10-18 18:40:12 +1300

    Refactored common code into viddl/steps.py
    Makes each script simpler and shorter.

---
+++ viddl/__init__.py
@@ -0,0 +1,7 @@
+###########################################################################
+#    Copyright (C) 2009 by Lee Begg                                      
+#    <llnz@paradise.net.nz>                                                             
+#
+# Copyright: See COPYING file that comes with this distribution
+#
+###########################################################################
